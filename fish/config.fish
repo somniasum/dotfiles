00c461ba2ddf8643aa79b458a25311c5b6b8c240
somniasum <somniasum@proton.me>
2026-04-25 22:59:52 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,14 @@
 #  Silence the greeting
 set -g fish_greeting ""
 
-#  Cargo / Rust
 fish_add_path ~/.cargo/bin
 
-#  Environment
 set -gx EDITOR zed
 set -gx VISUAL zed
 set -gx TERM ghostty
 set -gx COLORTERM truecolor
 
-#  Rust
-#  Cargo aliases
+# Cargo
 alias c "cargo run"
 alias b "cargo build"
 alias br "cargo build --release"
@@ -23,7 +20,7 @@ alias cu "cargo update"
 #  Zed
 alias z "zeditor ."
 
-#  Backlight
+#  Backlight script
 alias on "/usr/local/bin/backlight.sh on"
 alias off "/usr/local/bin/backlight.sh off"
 
@@ -60,18 +57,6 @@ alias gb "git branch"
 #  Misc utilities
 alias cat "bat --style=plain"
 alias grep "grep --color=auto"
-alias clr clear
 alias q exit
-alias path "echo $PATH | tr ' ' '\n'"
-alias ports "ss -tulpn"
 
-#  Zoxide (smart cd)
-abbr --erase z &>/dev/null
-alias z __zoxide_z
-
-abbr --erase zi &>/dev/null
-alias zi __zoxide_zi
-
-#  Shell prompt
-#  Starship prompt
 starship init fish | source
